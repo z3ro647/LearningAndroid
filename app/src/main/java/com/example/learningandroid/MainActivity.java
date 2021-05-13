c12800335a85e8d06f039d3b08363651b430e2ae
Vivek Kumar Gurung <z3ro647@gmail.com>
2021-05-13 23:03:08 +0545

    Imageview in Android Added

--- app/src/main/java/com/example/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtCounter;
-    private Button btnPlus, btnMinus, btnReset;
+    private Button btnPlus, btnMinus, btnReset, btnHideShow;
+    private ImageView ivMeme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlus = findViewById(R.id.btnPlus);
         btnMinus = findViewById(R.id.btnMinus);
         btnReset = findViewById(R.id.btnReset);
+        btnHideShow = findViewById(R.id.btn_hide_show);
+        ivMeme = findViewById(R.id.iv_meme);
 
         btnPlus.setOnClickListener(this);
         btnMinus.setOnClickListener(this);
         btnReset.setOnClickListener(this);
+
+        btnHideShow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(ivMeme.getVisibility() == View.VISIBLE){
+                    ivMeme.setVisibility(View.GONE);
+                }
+                else{
+                    ivMeme.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
 //        btnPlus.setOnClickListener(new View.OnClickListener() {
 //            @Override
